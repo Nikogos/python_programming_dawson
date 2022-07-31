6afcce6e4a0ca499b09405b8205a770e1729160a
Nikogos <I.Nikogosyan@yandex.ru>
2022-07-31 13:04:33 +0300

    bug: если в загаданном числе цифра повторяется и когда ты ее отгадал(повторяющуюся цифру), то он ее пишет только в Fermi, а в Pico не пишет.

--- deductive_game.py
+++ deductive_game.py
@@ -1,32 +1,39 @@
 import random
 
 number = str(random.randint(100, 1000))
-
-print(number)
-
-i = 0
 n = 0
 k = 0
+fermi = []
 pico = []
 bagels = []
-while i < 3:
-    i += 1
-    k += 1
-    while n < 10:
-        guess = input(f"отгадай {k}-ю цифру числа: ")
-        n += 1
-        if n < 10:
-            if guess == number[k - 1] :
-                print("\nFermi")
-                break;
-            elif guess == number[k] or guess == number[k + 1]:
-                pico += guess
-                print("\nPico")
+
+while k <= 3:
+    if k == 3 and n <= 10:
+        print(f"\nТы победил! Отгадал моё число {number}")
+        break;
+    elif k < 3 and n <= 10:
+        while n <= 10:
+            guess = input(f"\n\tУ тебя осталось {10 - n} попыток.\n\t Отгадай {k + 1}-ю цифру числа: ")
+            if n <= 10:
+                if guess == number[k]:
+                    k += 1
+                    fermi += guess
+                    print("\nFermi")
+                    print(f"\nFermi - {fermi} || Pico - {pico} || Bagels - {bagels}")
+                    break;
+                elif guess == number[k - 1] or guess == number[k - 2]:
+                    n += 1
+                    pico += guess
+                    print("\nPico")
+                else:
+                    n += 1
+                    bagels += guess
+                    print("\nBagels")
+                print(f"\nFermi - {fermi} || Pico - {pico} || Bagels - {bagels}")
             else:
-                bagels += guess
-                print("\nBagels")
-            print(f"\nPico - {pico}, Bagels - {bagels}")
-        else:
-            print("\nТы проиграл!")
-# print("\nТы победил!")
+                print("\nТы проиграл!")
+    else:
+        print("\nТы проиграл!!!!")
+
+
 input("\n\nДля выхода нажмите Enter")
