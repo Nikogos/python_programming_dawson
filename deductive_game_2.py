7adf00e83d97337072f348a82bdd7da9d9d44086
Nikogos <I.Nikogosyan@yandex.ru>
2022-08-03 22:09:21 +0300

    добал сообщение о проигрыше

--- deductive_game_2.py
+++ deductive_game_2.py
@@ -1,43 +1,55 @@
 import random
 
-number = ""
+# Глобальные переменные
+number = ""  # загаданное число
+tries = 0  # кол-во попыток
+count_digits = 3  # кол-во цифр в числе
+amount_tries = 10 #кол-во попыток
 i = 0
-k = 0
-while i < 3:
+while i < count_digits:
     numerical = str(random.randint(0, 9))
     if numerical not in number:
         number += numerical
         i += 1
-print(number)
-while k < 10:
-    guess = " "
-    answer = ["", "", ""]
-    k += 1
-    n = 0
-    while len(guess) != 3:
-        guess = input(f"попытка {k}: \n> ")
-    while n < 3:
-        for m in guess:
-            if m == number[n]:
-                answer[n] += "fermi"
-                n += 1
-            elif guess[n] == number[n - 1]: 
-                answer[n] += "pico"
-                n += 1
-            elif guess[n] == number[n - 2]:
-                answer[n] += "pico"
-                n += 1
-            else:
-                answer[n] += "bagels"
-                n += 1
-    print(answer)
-    if k == 10:
-        print("Ты проиграл!")
-    if answer[0] == answer[1] == answer[2] == "fermi":
-        print("Ты победил!")
-        k = 10
+print(f'Я загадал {count_digits}-x значное число. Попробуй угадать.\n')
+while tries < amount_tries:
+    tries += 1
 
+    guess = input(f"попытка {tries}: \n> ")
     
+    if tries == amount_tries and len(guess) != count_digits:
+        print(f"\nТы использовал {tries} попыток. Ты проиграл! \nЯ загадал число = {number}")
+        break
+        
+    if len(guess) != count_digits:
+        continue
+
+    if guess == number:
+        print('Ты выиграл')
+        break
+
+    answers = []  # массив подсказок
+    index = 0  # индекс ячейки в строчке
+    while index < count_digits:
+
+        if guess[index] == number[index]:
+            answers.append('fermi')
+        elif guess[index] in number:
+            answers.append('pico')
+
+        index += 1
+
+    answers.sort()
+    if answers:
+        print(' '.join(answers))
+    else:
+        print('bagels')
+      
+    if tries == amount_tries:
+        print(f"\nТы использовал {tries} потыток. Ты проиграл! \nЯ загадал число = {number}")
+
+
+
 input("\n\nнажмите Enter")
 
 
